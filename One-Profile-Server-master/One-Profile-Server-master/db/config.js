e75867f2d9005df38cf120243fc6710afc642029
beginner187 <ramesh.kumar.iitr@gmail.com>
2025-08-28 11:25:57 +0530

    final code of one profile project

---
+++ One-Profile-Server-master/One-Profile-Server-master/db/config.js
@@ -0,0 +1,6 @@
+const mongoose = require("mongoose");
+require("dotenv").config("../.env");
+
+mongoose.connect(
+  `mongodb+srv://harendraseervi1234:${process.env.PASS}@cluster0.ighny9l.mongodb.net/oneprofile`
+);
